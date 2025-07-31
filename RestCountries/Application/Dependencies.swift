aad300789711f074cdd7a902195997c994054509
panther222128 <commando77@naver.com>
2025-07-31 19:47:58 +0900

    dependencies

--- RestCountries/Application/Dependencies.swift
+++ RestCountries/Application/Dependencies.swift
@@ -10,14 +10,8 @@ import ComposableArchitecture
 
 enum CountriesRepositoryKey: DependencyKey {
     static var liveValue: CountriesRepository {
-        lazy var apiBaseURL: URL = {
-            guard let urlString = Bundle.main.infoDictionary?["API_BASE_URL"] as? String else {
-                fatalError("URL Error")
-            }
-            return URL(string: urlString)!
-        }()
-        let repository = DefaultCountriesRepository(dataTransferService: DefaultDataTransferService(with: DefaultNetworkService(configuration: ApiDataNetworkConfig(baseURL: apiBaseURL))))
-        return repository
+        @Dependency(\.dataTransferService) var dataTransferService
+        return DefaultCountriesRepository(dataTransferService: dataTransferService)
     }
 }
 
@@ -25,5 +19,35 @@ extension DependencyValues {
     var countriesRepository: CountriesRepository {
         get { self[CountriesRepositoryKey.self] }
         set { self[CountriesRepositoryKey.self] = newValue }
+    }
+}
+
+enum DataTransferServiceKey: DependencyKey {
+    static var liveValue: DataTransferService {
+        @Dependency(\.networkService) var networkService
+        return DefaultDataTransferService(with: networkService)
+    }
+}
+
+extension DependencyValues {
+    var dataTransferService: DataTransferService {
+        get { self[DataTransferServiceKey.self] }
+        set { self[DataTransferServiceKey.self] = newValue }
+    }
+}
+
+enum NetworkServiceKey: DependencyKey {
+    static var liveValue: NetworkService {
+        let apiBaseURL = URL(string: Bundle.main.infoDictionary?["API_BASE_URL"] as? String ?? "")!
+        let config = ApiDataNetworkConfig(baseURL: apiBaseURL)
+        let networkService = DefaultNetworkService(configuration: config)
+        return networkService
+    }
+}
+
+extension DependencyValues {
+    var networkService: NetworkService {
+        get { self[NetworkServiceKey.self] }
+        set { self[NetworkServiceKey.self] = newValue }
     }
 }
